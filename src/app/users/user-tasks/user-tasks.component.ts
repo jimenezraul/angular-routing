2693e84357b447ce1f033721a7fa67b4f7929f74
Raul <jimenezraul1981@gmail.com>
2025-01-25 15:58:27 -0500

    add user tasks component and update routing for user-specific tasks

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
+import { UsersService } from '../users.service';
+import type { User } from '../user/user.model';
+import { TasksService } from '../../tasks/tasks.service';
 
 @Component({
   selector: 'app-user-tasks',
@@ -6,4 +9,11 @@ import { Component } from '@angular/core';
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {}
+export class UserTasksComponent {
+  id = input.required<string>();
+  private userService = inject(UsersService);
+  private taskService = inject(TasksService);
+
+  user = computed(() => this.userService.users.find((user: User) => user.id === this.id()) ?? null);
+  tasks = computed(() => this.taskService.allTasks().filter((task) => task.userId === this.id()));
+}
--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,9 +1,11 @@
 import { Component, computed, input } from '@angular/core';
 
 import { type User } from './user.model';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-user',
+  imports: [RouterLink, RouterLinkActive],
   standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
