ac2c514a5e21cccd87f54d2ee3055a4f9e373065
Raul <jimenezraul1981@gmail.com>
2025-01-25 21:42:18 -0500

    refactor routing structure by moving user task routes to a separate file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,7 @@
 import { Routes } from "@angular/router";
-import { TasksComponent } from "./tasks/tasks.component";
+import { routes as userRoutes } from "./users/user.routes";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 
 export const routes: Routes = [
@@ -13,21 +12,7 @@ export const routes: Routes = [
     {
         path: 'users/:id',
         component: UserTasksComponent,
-        children: [
-            {
-                path: '',
-                redirectTo: 'tasks',
-                pathMatch: 'prefix'
-            },
-            {
-                path: 'tasks',
-                component: TasksComponent
-            },
-            {
-                path: 'new-task',
-                component: NewTaskComponent
-            }
-        ]
+        children: userRoutes
     },
     {
         path: '**',
