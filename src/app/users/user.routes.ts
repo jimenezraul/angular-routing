961e697acc8dd6b2c3c526acf2e74b28f5c38e24
Raul <jimenezraul1981@gmail.com>
2025-01-27 12:30:10 -0500

    refactor user routes to include TasksService as a provider and restructure child routes

--- src/app/users/user.routes.ts
+++ src/app/users/user.routes.ts
@@ -1,24 +1,31 @@
 import { Routes } from "@angular/router";
 import { TaskComponentResolver, UserNameResolver } from "../tasks/tasks.resolver";
+import { TasksComponent } from "../tasks/tasks.component";
+import { TasksService } from "../tasks/tasks.service";
 
 export const routes: Routes = [
     {
         path: '',
-        redirectTo: 'tasks',
-        pathMatch: 'prefix'
-    },
-    {
-        path: 'tasks',
-        loadComponent: () =>
-            import('../tasks/tasks.component').then(m => m.TasksComponent),
-        resolve: {
-            userTasks: TaskComponentResolver
-        },
-        title: UserNameResolver
-    },
-    {
-        path: 'new-task',
-        loadComponent: () =>
-            import('../tasks/new-task/new-task.component').then(m => m.NewTaskComponent)
+        providers: [TasksService],
+        children: [
+            {
+                path: '',
+                redirectTo: 'tasks',
+                pathMatch: 'prefix'
+            },
+            {
+                path: 'tasks',
+                component: TasksComponent,
+                resolve: {
+                    userTasks: TaskComponentResolver
+                },
+                title: UserNameResolver
+            },
+            {
+                path: 'new-task',
+                loadComponent: () =>
+                    import('../tasks/new-task/new-task.component').then(m => m.NewTaskComponent)
+            }
+        ]
     }
 ]
